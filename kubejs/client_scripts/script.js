eb466138c2195ca9a817872b2f6f2190e77ef797
PreciseModpacker <precise.modpacker@gmail.com>
2023-01-14 22:32:26 +0100

    Add changes in the KubeJS

--- kubejs/client_scripts/script.js
+++ kubejs/client_scripts/script.js
@@ -5,4 +5,7 @@ console.info('Hello, World! (You will see this line every time client resources 
 onEvent('jei.hide.items', event => {
 	// Hide items in JEI here
 	// event.hide('minecraft:cobblestone')
+		event.hide('@waystones'),
+		event.hide('charm:kiln')
 })
+
